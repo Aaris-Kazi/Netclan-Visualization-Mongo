81ce5487e0944325093d9527397327f8c6700676
Aaris-Kazi <aariskazi@gmail.com>
2023-03-04 21:51:32 +0530

    js complete

--- static_files/fllterLink.js
+++ static_files/fllterLink.js
@@ -1,5 +1,17 @@
 function filter_direction() {
     const filter = document.getElementsByClassName('form-select'); 
     console.log(filter[0].value);
-    
+    const search = document.getElementById("dashboard-name");
+    console.log(search.innerHTML)
+    const button = document.createElement('a');
+    button.setAttribute('href', "/filter?search="+search.innerHTML+"&&filter="+filter[0].value);
+    button.click();
+    // $.get("/filter?search="+search.innerHTML+"&&filter="+filter[0].value, function (response) {
+    //     if (response) {
+    //         console.log('success')
+    //     } else {
+    //         console.log('not success')
+            
+    //     }
+    // });
 }
