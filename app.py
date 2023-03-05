c14bff0eb63d611c457da77790f71ae2a41a1adf
Aaris-Kazi <aariskazi@gmail.com>
2023-03-05 12:06:23 +0530

    beautifying changes

--- app.py
+++ app.py
@@ -1,30 +1,15 @@
 from flask import Flask, redirect, render_template, url_for, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
-from mongo import show_sector, var_graph, sumofAll, sumofFilter
+from mongo import  sumofAll, sumofFilter
 
 app = Flask(__name__)
 CORS(app)
 
-sector_name, sector_size = show_sector()
-
-@app.route('/graph')
-def index():
-    return render_template('index.html', name = sector_name, size = sector_size)
 
 @app.route('/')
 def graph():
     return render_template('index.html')
-
-@app.route('/graph_operation', methods = ["GET","POST"])
-def graph_op():
-    var = request.form["variables"]
-    fil = request.form["filters"]
-    x, y = var_graph(var, fil)
-    variables = ['intensity', 'likelihood', 'relevance', 'country', 'topic', 'region']
-    filters = ['end_year', 'topic', 'sector', 'region', 'pestle', 'country', 'city']
-    # print(x,y)
-    return render_template('graph.html', var = variables, filters = filters, name = x, size = y, topic=var)
 
 
 @app.route('/search', methods = ["GET","POST"])
@@ -35,8 +20,8 @@ def search():
         return render_template('index.html', topic = search, x = x, y = y, x_label = search, y_label = 'sum')
     else:
         return redirect("/")
+    
 
-# http://127.0.0.1:5000/filter?search=RELEVANCE&&filter=end_year
 @app.route('/filter', methods = ["GET","POST"])
 def filter():
     if request.method == "GET":
@@ -47,7 +32,7 @@ def filter():
         filter = filter.replace(" ", "_")
         x,y = sumofFilter(str(search).lower(), filter.lower())
         return render_template('index.html', topic = search, x = x, y = y, x_label = search, y_label = filter)
-    # return redirect('/')
+    
     
 
 if __name__ == '__main__':
